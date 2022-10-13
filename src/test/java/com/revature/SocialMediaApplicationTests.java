45f6b11af6ebe828e572d958715f81296a45901d
alopez651 <ana.lopez6517@gmail.com>
2022-10-13 11:17:21 -0700

    aws

--- src/test/java/com/revature/SocialMediaApplicationTests.java
+++ src/test/java/com/revature/SocialMediaApplicationTests.java
@@ -2,8 +2,10 @@ package com.revature;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
+@PropertySource("classpath:prod.properties")
 class SocialMediaApplicationTests {
 
 	@Test

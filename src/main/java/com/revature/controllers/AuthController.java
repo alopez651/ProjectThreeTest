5589ff21572ed207f1b37531672dfbb2360968c8
Ana <81542559+alopez651@users.noreply.github.com>
2022-10-15 18:42:35 -0700

    Update AuthController.java

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -14,7 +14,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 
 public class AuthController {
 

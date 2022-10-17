c5f971bdff2d4266aa4aa73102a7f65e120465e0
Ana <81542559+alopez651@users.noreply.github.com>
2022-10-17 10:00:48 -0700

    Update UserController.java

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@CrossOrigin(origins = "http://maxwellfrontend.s3-website-us-east-1.amazonaws.com", allowCredentials = "true")
 //@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
